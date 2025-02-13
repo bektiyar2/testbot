9599908b1488f45c811807f44cea738cc1f81d36
bektiyar2 <138044802+bektiyar2@users.noreply.github.com>
2025-02-13 15:19:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, MessageHandler, Filters
 # Функция-обработчик для переадресации сообщений
 def forward_message(update, context):
     # ID группы, в которую будут переадресовываться сообщения
-    destination_group_id = -1001927385577
+    destination_group_id = () #указать ID группы, в которую будет переадресация сообщения
     message = update.message
     if message.text:
         text = message.text.lower()
@@ -16,7 +16,7 @@ def error(update, context):
     print(f"Ошибка: {context.error}")
 
 # Создаем экземпляр класса Updater и передаем ему токен вашего бота
-updater = Updater(token='6081009857:AAFZcVVHTX7mCXPpzWj7S3AJ0KlK4bQ21vs', use_context=True)
+updater = Updater(token='номер токена', use_context=True)
 
 # Получаем экземпляр диспетчера, чтобы зарегистрировать обработчики
 dispatcher = updater.dispatcher
